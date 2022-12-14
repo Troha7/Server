c19b2ac5d4611c32d77dadfecded7aa1a9b6eb0d
Trotsenko <trohae7@gmail.com>
2022-12-14 23:03:25 +0200

    Add receiveFile

--- src/main/java/ua/hillelit/lms/MyServer.java
+++ src/main/java/ua/hillelit/lms/MyServer.java
@@ -1,6 +1,8 @@
 package ua.hillelit.lms;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -43,6 +45,9 @@ public class MyServer {
   private static class ClientHandler extends Thread {
 
     private final Socket clientSocket;
+    private static BufferedReader textReader;
+    private static PrintWriter textWriter;
+    private static DataInputStream fileReader;
     private static int id;
 
     public ClientHandler(Socket socket) {
@@ -51,24 +56,24 @@ public class MyServer {
 
     public void run() {
       try {
-        BufferedReader in = new BufferedReader(
-            new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        textReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        textWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+        fileReader = new DataInputStream(clientSocket.getInputStream());
 
-        clients.add(out);
+        clients.add(textWriter);
         id++;
 
         writeToAllClients();
 
-        readFromClient(in);
+        readFromClient();
 
-        closeClientSocket(in, out);
+        closeClientSocket();
 
         System.out.println("[SERVER] Client-" + id + " is exit");
-        clients.remove(out);
+        clients.remove(textWriter);
         id--;
 
-      } catch (IOException e) {
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
@@ -79,24 +84,45 @@ public class MyServer {
       }
     }
 
-    private void readFromClient(BufferedReader in) throws IOException {
+    private void readFromClient() throws Exception {
       String inputLine;
-      while ((inputLine = in.readLine()) != null) {
+      while ((inputLine = textReader.readLine()) != null) {
+
+        if (inputLine.startsWith("exit")) {
+          break;
+        }
+
+        String pathToFile;
+        if (inputLine.startsWith("file")) {
+          pathToFile = inputLine.replaceFirst("file", "").trim();
+          receiveFile(pathToFile);
+        }
 
         String time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
         String socket = clientSocket.getRemoteSocketAddress().toString();
 
         System.out.println("[Client-" + id + "] " + time + socket + " - " + inputLine);
-
-        if (inputLine.equalsIgnoreCase("exit")) {
-          break;
-        }
       }
     }
 
-    private void closeClientSocket(BufferedReader in, PrintWriter out) throws IOException {
-      out.close();
-      in.close();
+    private static void receiveFile(String pathToFile) throws Exception {
+      FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
+
+      long size = fileReader.readLong();     // read file size
+      byte[] buffer = new byte[4 * 1024];
+      int minSize = (int) Math.min(buffer.length, size);
+      int bytes = fileReader.read(buffer, 0, minSize);
+      while (size > 0 && bytes != -1) {
+        fileOutputStream.write(buffer, 0, bytes);
+        size -= bytes;      // read up to file size
+      }
+      fileOutputStream.close();
+    }
+
+    private void closeClientSocket() throws IOException {
+      textWriter.close();
+      textReader.close();
+      fileReader.close();
       clientSocket.close();
     }
 
