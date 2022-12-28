35c8ad4e6df8c3df591c7a33c17754ac6a9d5d68
Trotsenko <trohae7@gmail.com>
2022-12-28 23:25:35 +0200

    Add javadoc

--- src/main/java/ua/hillelit/lms/FileReceiver.java
+++ src/main/java/ua/hillelit/lms/FileReceiver.java
@@ -23,7 +23,7 @@ public class FileReceiver {
    * First we create {@link FileOutputStream} on the path {@code pathToFile},
    * then get file size from the client.
    * Create a new buffer of size 4 Kbs.
-   * Send the file of 4 Kbs in a loop.
+   * Get the file of 4 Kbs in a loop.
    *
    * @param pathToFile path to file
    * @throws Exception
@@ -32,14 +32,12 @@ public class FileReceiver {
     FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
     fileReader = new DataInputStream(clientSocket.getInputStream());
 
-    int bytes = 0;
-    long size = 0;
-    size = fileReader.readLong();     // read file size
+    long size = fileReader.readLong();     // read file size
     System.out.println("[SERVER] >>> file size[" + size +"]");
 
     byte[] buffer = new byte[4 * 1024]; // buffer size 4 Kbs
     int minSize = (int) Math.min(buffer.length, size);
-    bytes = fileReader.read(buffer, 0, minSize);
+    int bytes = fileReader.read(buffer, 0, minSize);
     while (size > 0 && bytes != -1) {
       fileOutputStream.write(buffer, 0, bytes);
       size -= bytes; // read up to file size
